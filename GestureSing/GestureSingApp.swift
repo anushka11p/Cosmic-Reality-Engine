e43317822ade229a9f41c51feeb63090941d26ea
anushka <anushka11@example.com>
2026-07-26 16:49:07 +0530

    Initial Commit

---
+++ GestureSing/GestureSingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureSingApp.swift
+//  GestureSing
+//
+//  Created by Anu on 26/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GestureSingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
